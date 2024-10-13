4f75e2f84acc067e14074f1b22f17592736f44d4
Mukul Kumar Rajak <mkumarrajak@adobe.com>
2024-10-14 01:25:46 +0530

    email sent implemented

--- notification-service/src/main/java/com/work/notification/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/work/notification/config/KafkaConsumerConfig.java
@@ -1,14 +1,17 @@
 package com.work.notification.config;
 
+import com.work.notification.dto.WinnerDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +27,11 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(consumerConfigs());
     }
+//    @Bean
+//    public ConsumerFactory<String, WinnerDTO> consumerFactory() {
+//        return new DefaultKafkaConsumerFactory<>(consumerConfigs(), new StringDeserializer(),
+//                new JsonDeserializer<>(WinnerDTO.class));
+//    }
 
     @Bean
     public Map<String, Object> consumerConfigs() {
@@ -32,6 +40,8 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getGroupId());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class); // Updated to use ErrorHandlingDeserializer
+//        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class); // Set JsonDeserializer as the delegate
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getAutoOffsetReset());
         return props;
     }
@@ -42,4 +52,12 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
+
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, WinnerDTO> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, WinnerDTO> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
 }
