37d09603d24acfb30f866e59b3255d183327c403
Mukul Kumar Rajak <mkumarrajak@adobe.com>
2024-10-14 01:24:23 +0530

    correct user

---
+++ scheduler-service/src/test/java/com/work/scheduler/SchedulerServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.work.scheduler;
+
+import com.work.scheduler.SchedulerApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SchedulerApplication.class)
+class SchedulerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
